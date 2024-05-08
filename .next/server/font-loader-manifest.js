a1b5bbf59d1dd44a99160dc8764a9d77300c3c23
jitensp <plaicesaijp@gmail.com>
2024-05-08 18:56:21 +0100

    url update 2

--- .next/server/font-loader-manifest.js
+++ .next/server/font-loader-manifest.js
@@ -1,1 +1,8 @@
-self.__FONT_LOADER_MANIFEST={pages:{"/_app":["static/media/1864ddf2dca1230f.p.otf"]},app:{}};
+self.__FONT_LOADER_MANIFEST={
+  "pages": {
+    "/_app": [
+      "static/media/1864ddf2dca1230f.p.otf"
+    ]
+  },
+  "app": {}
+}
